b7426764f75dbe83ef05c557453c199eee49ec29
diego.neves <diego.neves@pti.org.br>
2016-06-17 17:13:25 -0300

    Zabbix Switch Screen - Create a xml file to import on zabbix screen

--- ZabbixSwScreen.sh
+++ ZabbixSwScreen.sh
@@ -5,9 +5,14 @@
 # 
 # Diego Neves  - <diego@diegoenves.eti.br - github.com/diegoaceneves/
 # 
-# LICENSE - GPL3.0
 #
-# 2016
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+# ----------------------------------------------------------------------------
+#
 
 
 if [ $# != 3  ] ; then
